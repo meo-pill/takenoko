1b4d7907b62e0064774d241fb8f0b97ffcee5f97
meo <83820503+meo-pill@users.noreply.github.com>
2022-01-31 14:28:13 +0100

    Update index.h

--- fichier_h/index.h
+++ fichier_h/index.h
@@ -3,6 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../lib/SDL2/include/SDL2/SDL.h"
-#include "struct.h"
+#include "struct.h" // on ne met rien au dessu ces 4 la (il on tout ce sur quoi le programe repose)
 #include "file.h"
 #include "deplacement.h"
