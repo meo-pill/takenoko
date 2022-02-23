d087ae3e2ffad35eafbfe198b5216a14bb8894fb
Lunt <97598276+ForLunt@users.noreply.github.com>
2022-02-23 14:12:55 +0100

    pour quoi??????
    nooooooooooon

--- src/fenetre.c
+++ src/fenetre.c
@@ -1,17 +1,13 @@
+#include <unistd.h>
 #include "../lib/Creation.h"
+#include "../lib/menu.h"
 
 void affiche_option(SDL_Window* pWindow,SDL_Renderer *renderer,int W,int H){
 	char fond_option[]="image/en_plus/Fond_option.png";
 
-	SDL_RWops *rwop=NULL;//permet d'incruster l'image sur la fenètre
-
-	SDL_Surface* pSprite =NULL;//ajout d'une image par dessus le fond
-
 	SDL_Texture* pTexture =NULL;//pour ajouter une animation
 	SDL_Texture *  bouton =NULL;
 	SDL_Texture *  bouton2 =NULL;
-
-	//SDL_Rect fix={138,0,W-138,H};
 
 	SDL_Color Noir = {0 , 0 , 0};
 
@@ -19,6 +15,8 @@ void affiche_option(SDL_Window* pWindow,SDL_Renderer *renderer,int W,int H){
 	SDL_Rect tailBouton2;
 
 	int h=1,x=0,y=0;
+
+	printf("Ok1\n");
 
 	bouton =CreationText(renderer,&tailBouton,"image/police/Takenoko.TTF",30,TTF_STYLE_BOLD,"Quiter",Noir,580,750);
 	if ( bouton == NULL ){
@@ -30,41 +28,50 @@ void affiche_option(SDL_Window* pWindow,SDL_Renderer *renderer,int W,int H){
 		exit ( EXIT_FAILURE );
 	}
 
-	rwop=SDL_RWFromFile(fond_option,"rb");
-	pSprite = IMG_LoadBMP_RW(rwop);
-	if(pSprite==NULL)
+	printf("Ok2\n");
+	pTexture = IMG_LoadTexture(renderer, fond_option);
+	if(pTexture==NULL){
+		fprintf ( stderr , " Erreur au niveau de l'image: %s \n " , TTF_GetError ());
 		exit ( EXIT_FAILURE );
-	rwop->close(rwop);
-	rwop=NULL;
-	pTexture = SDL_CreateTextureFromSurface(renderer,pSprite);//X=138;Y=4
-	if(pTexture==NULL)
-		exit ( EXIT_FAILURE );
+	}
+
+	printf("Ok3\n");
+	while(h<H){
+		//permet de faire bouger l'animation
+		h+=100;
+		printf("ok4 h=%d\n",h);
+		SDL_Rect annimation = { 1,h,W-138,H};
+		SDL_Rect crop={138,H,W-138, H };
+		//on pose l'animation sur la fenêtre
+		SDL_RenderCopy(renderer,pTexture,&annimation,&crop);// Copie du sprite grâce au SDL_Renderer
+		SDL_RenderPresent(renderer);
+		sleep(5);
+	}
+	
 	while(1){
 		Uint32 Clic = SDL_GetMouseState(&x,&y);
 		//création de la "fenêtre ou nous verons une partie de l'image
-		if(h<H){
-			//permet de faire bouger l'animation
-			Uint32 ticks = SDL_GetTicks();
-			Uint32 seconds = ticks / 230;
-			Uint32 sprite = seconds % 4;
-			x=sprite*120;
-			SDL_Rect annimation = { 138,h,W-138,h};
-			SDL_Rect crop={138,0, 120, h };
-			//on pose l'animation sur la fenêtre
-			SDL_RenderCopy(renderer,pTexture,&annimation,&crop);// Copie du sprite grâce au SDL_Renderer
-		}
-		else{
-			SDL_RenderCopy(renderer, pTexture, NULL, NULL);
-			if((x>=tailBouton.x && x<=(tailBouton.w+tailBouton.x)) && (y>=tailBouton.y && y<=(tailBouton.h+tailBouton.y))){
-				if(Clic==1)
-					return;
-				else
-					SDL_RenderCopy(renderer, bouton2,NULL,&tailBouton2);
+	
+		SDL_RenderCopy(renderer, pTexture, NULL, NULL);
+		if((x>=tailBouton.x && x<=(tailBouton.w+tailBouton.x)) && (y>=tailBouton.y && y<=(tailBouton.h+tailBouton.y))){
+			if(Clic==1){
+				if(NULL!=bouton) 
+					SDL_DestroyTexture(bouton);
+				if(NULL!=bouton2) 
+					SDL_DestroyTexture(bouton2);
+				if(NULL!=pTexture)
+					SDL_DestroyTexture(pTexture);
+				menu();
+
 			}
 			else
-				SDL_RenderCopy(renderer, bouton,NULL,&tailBouton);
+				SDL_RenderCopy(renderer, bouton2,NULL,&tailBouton2);
 		}
+		else
+			SDL_RenderCopy(renderer, bouton,NULL,&tailBouton);
+	
 		//présentation final
 		SDL_RenderPresent(renderer);
+		printf("option: %d %d\n",x,y);
 	}
 }
