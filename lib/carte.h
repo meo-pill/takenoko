c98dceda77012a9a096d085cd4634909e32d738d
Nail Leo <s193670@ic2s114-12.univ-lemans.fr>
2022-03-02 09:56:57 +0100

    un nouvel espoir

---
+++ lib/carte.h
@@ -0,0 +1,7 @@
+#include "index.h"
+
+int verif_panda (carte_t const * carte, joueur_t const * joueur);
+
+int verif_parcelle_triangle (couleur_E couleur);
+
+carte_t * creer_carte(char * type, char * descrip, char * image, int pt );
